f8060da49e023e503bee95c7e2e896642b80de7a
Tangent 128 <Tangent128@gmail.com>
2018-03-23 18:48:49 -0400

    Buffering the file is unnecessary actually

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,13 @@ pub struct SpeeqeFormat(Vec<String>, i8);
 #[cfg(test)]
 mod tests {
     use serde_json;
-    use std::io::Read;
     use std::fs::File;
     use ::SpeeqeFormat;
 
     #[test]
     fn parse_speeqe_format() {
-        let mut speeqe_file = File::open("src/data/speeqe.json").unwrap();
-
-        let mut speeqe_json = String::new();
-        speeqe_file.read_to_string(&mut speeqe_json).unwrap();
-
-        let speeqe_data: Vec<SpeeqeFormat> = serde_json::from_str(&speeqe_json).unwrap();
+        let speeqe_file = File::open("src/data/speeqe.json").unwrap();
+        let speeqe_data: Vec<SpeeqeFormat> = serde_json::from_reader(&speeqe_file).unwrap();
 
         assert_eq!(speeqe_data[0].0[0], "Missingno.");
         // NidoranM is always male, gender ratio value = 8
