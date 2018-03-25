e38314f7e143f98887774802d342e5709845df12
Tangent 128 <Tangent128@gmail.com>
2018-03-25 01:23:29 -0400

    deserialize YAML format

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,24 @@ extern crate serde_derive;
 
 extern crate serde;
 extern crate serde_json;
+extern crate serde_yaml;
 
 /// The shape of the list of Pokémon used in the original Speeqe-based name generator
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SpeeqeFormat(pub Vec<String>, pub i8);
 
 /// The shape of the new Pokémon list format
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Species {
     pub names: Vec<String>,
     pub gender: Gender,
 
     #[serde(skip_serializing_if = "Vec::is_empty")]
+    #[serde(default = "Vec::new")]
     pub tags: Vec<SpeciesTag>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum Gender {
     Agender,
     Male,
@@ -27,7 +29,7 @@ pub enum Gender {
 }
 
 /// Tags indicating a species is eligable for certain specific modifiers
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum SpeciesTag {
     Mega,
     MegaXY,
@@ -38,8 +40,9 @@ pub enum SpeciesTag {
 #[cfg(test)]
 mod tests {
     use serde_json;
+    use serde_yaml;
     use std::fs::File;
-    use ::SpeeqeFormat;
+    use ::*;
 
     #[test]
     fn parse_speeqe_format() {
@@ -50,5 +53,22 @@ mod tests {
         // NidoranF is always female, gender ratio value = 8
         assert_eq!(speeqe_data[29].1, 8);
         assert_eq!(speeqe_data[151].0[0], "Mew");
+    }
+
+    #[test]
+    fn parse_species_format() {
+        let species_file = File::open("src/data/species.yaml").unwrap();
+        let species_data: Vec<Species> = serde_yaml::from_reader(&species_file).unwrap();
+
+        assert_eq!(species_data[0].names[0], "Missingno.");
+        // Charmander has a defined gender ratio
+        assert_eq!(species_data[6].gender, Gender::Ratio(0.125));
+        // NidoranF is always female
+        assert_eq!(species_data[29].gender, Gender::Female);
+        // NidoranM is always male
+        assert_eq!(species_data[32].gender, Gender::Male);
+        // Mew is Agender
+        assert_eq!(species_data[151].gender, Gender::Agender);
+        assert_eq!(species_data[151].names[0], "Mew");
     }
 }
