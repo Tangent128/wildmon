8c8859edbbf5ab01df75f4bf236812d605d9f672
Tangent Wantwight <tangent128@gmail.com>
2021-03-28 23:07:14 -0400

    factor out name/gender selection logic

--- src/generator.rs
+++ src/generator.rs
@@ -4,6 +4,32 @@ use rand::{seq::SliceRandom, Rng};
 const MISSINGNO: &'static str = "Missingno.";
 static DEFAULT_GENDERS: &[Gender] = &[Gender::Male, Gender::Female, Gender::Agender];
 
+fn pick_name<'r, 's, R: Rng + ?Sized>(_rng: &'r mut R, species: &'s Species) -> &'s str {
+    match species.names.first() {
+        Some(name) => name.as_ref(),
+        None => MISSINGNO,
+    }
+}
+fn pick_gender<R: Rng + ?Sized>(
+    rng: &mut R,
+    species: &Species,
+    allow_genders: &Vec<Gender>,
+) -> Gender {
+    let allowed_genders = match allow_genders.len() {
+        0 => DEFAULT_GENDERS,
+        _ => &allow_genders,
+    };
+    let gender = species.gender.randomize(rng);
+    if allowed_genders.contains(&gender) {
+        gender
+    } else {
+        allowed_genders
+            .choose(rng)
+            .copied()
+            .unwrap_or(Gender::Agender)
+    }
+}
+
 pub fn wildmon<R: Rng + ?Sized>(
     rng: &mut R,
     pokedex: &Vec<Species>,
@@ -15,24 +41,9 @@ pub fn wildmon<R: Rng + ?Sized>(
         Some(species) => species,
         None => return MISSINGNO.into(),
     };
-    let name = match species.names.first() {
-        Some(name) => name.as_ref(),
-        None => MISSINGNO,
-    };
 
-    let allowed_genders = match opts.allow_genders.len() {
-        0 => DEFAULT_GENDERS,
-        _ => &opts.allow_genders,
-    };
-    let mut gender = species.gender.randomize(rng);
-    if !allowed_genders.contains(&gender) {
-        gender = opts
-            .allow_genders
-            .choose(rng)
-            .copied()
-            .unwrap_or(Gender::Agender);
-    }
-
+    let name = pick_name(rng, species);
+    let gender = pick_gender(rng, species, &opts.allow_genders);
     let level = rng.gen_range(1..=100);
 
     let mut mon = format!("Wild {}{} (lv{})", name, gender.symbol(), level);
